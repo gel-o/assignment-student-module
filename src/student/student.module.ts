915d83655db813b88f371a7735b893b562a452c1
bgnayat <131599772+bgnayat@users.noreply.github.com>
2025-01-11 14:13:18 +0800

    done create student

---
+++ src/student/student.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Student } from './entities/student.entity';
+import { StudentService } from './student.service';
+import { StudentController } from './student.controller';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Student]) // Register the Student entity
+  ],
+  controllers: [StudentController],
+  providers: [StudentService],
+})
+export class StudentModule { }
+export class Appmodule { }
