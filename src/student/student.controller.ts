887cd64d9e1b9926f0452538316f1510768ba38a
bgnayat <131599772+bgnayat@users.noreply.github.com>
2025-01-11 14:07:10 +0800

    added mysql details

---
+++ src/student/student.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
+import { StudentService } from './student.service';
+import { Student } from './entities/student.entity';
+
+@Controller('students')
+export class StudentController {
+    constructor(private readonly studentService: StudentService) { }
+
+    // Endpoint to create a student
+    @Post()
+    create(@Body() studentData: Partial<Student>): Promise<Student> {
+        return this.studentService.create(studentData);
+    }
+
+    // Endpoint to fetch all students
+    @Get()
+    findAll(): Promise<Student[]> {
+        return this.studentService.findAll();
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: number, @Body() updateData: Partial<Student>) {
+        return this.studentService.update(id, updateData);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: number) {
+        return this.studentService.remove(id);
+    }
+
+
+}
